7836f1cb8a29ebe25e6e6fb5d9de2f41e968bb31
ali hossam <90430372+alihossam8600@users.noreply.github.com>
2025-11-12 15:26:29 +0200

    Define Animal class with member functions and properties

--- Animal.h
+++ Animal.h
@@ -1,11 +1,32 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
-#include <string>
+#include <iostream>
 using namespace std;
 
-//define animal class here 
+class Animal{
+private:
+    string name;
+    int age;
+    bool isHungry;
+public:
+    Animal();
+    Animal(string n,int ag,bool hungry);
+    ~Animal();
+
+    
+    
+    void setname(string n);
+    void setage(int ag);
+    void sethungry(bool hungry);
+    string getname();
+    int getage();
+    bool gethungry();
+
+    void display();
+    void feed();
 
 
+};
 
-#endif
+
+#endif 
