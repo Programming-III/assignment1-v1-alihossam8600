2f4f42c4e41510285c28567908c7ee14b4342351
ali hossam <90430372+alihossam8600@users.noreply.github.com>
2025-11-12 15:28:11 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,24 @@
 #ifndef REPTILE_H
 #define REPTILE_H
-
 #include "Animal.h"
+#include <iostream>
 
-//define reptile class here
+class Reptile:public Animal{
+private:
+    bool isVenomous;
+
+public:
+    Reptile();
+    Reptile(string n,int ag,bool hungry,bool ven);
+    ~Reptile();
+
+    void setven(bool v);
+    bool getven();
+
+};
 
 
 
 
 
-
-
-
-
-#endif
+#endif 
