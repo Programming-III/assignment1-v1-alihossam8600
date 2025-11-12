5dba8b0d34d9fdc5adf149e191ffc83bf2538611
ali hossam <90430372+alihossam8600@users.noreply.github.com>
2025-11-12 15:27:23 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,9 +1,25 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
 #include "Animal.h"
+#include <iostream>
 
-//define enclosure class here 
+class Enclosure{
+private:
+
+    Animal* animal;
+    int capacity;
+    int currentCount;
+
+public:
+    Enclosure();
+    Enclosure(int cap,int current);
+    ~Enclosure();
+    void setcap(int cap);
+    void setcurrent(int current);
+    int getcap();
+    int getcurrent();
+    void addAnimal(Animal* a);
+    void displayAnimals();
 
 
 
@@ -11,5 +27,6 @@
 
 
 
+};
 
 #endif
