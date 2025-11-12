6bb693ef6249d428a1762f136034407602ac765a
ali hossam <90430372+alihossam8600@users.noreply.github.com>
2025-11-12 15:26:55 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,12 +1,23 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
+#include <iostream>
 
-//define class bird here 
+class Bird:public Animal{
+private:
+    float wingSpan;
+
+public:
+    Bird();
+    Bird(string n,int ag,bool hungry,float span);
+    ~Bird();
+
+    void setspan(float s);
+    float getspan();
 
 
 
+};
 
 
 
